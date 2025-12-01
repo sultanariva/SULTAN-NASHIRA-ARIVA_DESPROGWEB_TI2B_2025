4a902a3d4bca00e21000fd91d725c6aaa9eaaddf
Sultan Nashira Ariva <sultannanda55@gmail.com>
2025-12-01 08:28:04 +0700

    Praktikum4_Jobsheet10.2

--- dasarWeb/Jobsheet10/proses.php
+++ dasarWeb/Jobsheet10/proses.php
@@ -29,10 +29,22 @@ if ($aksi == 'tambah') {
     } else {
         echo "Gagal mengubah data: " . pg_last_error($koneksi);
     }
-}
-else {
+} elseif ($aksi == 'hapus') {
+    if (isset($_GET['id'])) {
+        $id = $_GET['id'];
+    }
+
+    $query = "DELETE FROM anggota WHERE id = $id";
+    
+    if (pg_query($koneksi, $query)) {
+        header("Location: index.php");
+        exit();
+    } else {
+        echo "Gagal menghapus data: " . pg_last_error($koneksi);
+    }
+} else {
     echo "ID tidak valid.";
-}
+} 
 
 pg_close($koneksi);
 
