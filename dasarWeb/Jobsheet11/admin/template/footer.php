ee1ea1c038b42eff37e07afdafd2ce2cb0ed50c7
Sultan Nashira Ariva <sultannanda55@gmail.com>
2025-12-10 10:31:01 +0700

    Praktikum3_Jobsheet11

---
+++ dasarWeb/Jobsheet11/admin/template/footer.php
@@ -0,0 +1,5 @@
+<script src="assets/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/chart.js@4.3.2/dist/chart.umd.js" integrity="sha384-eI7PSr3L1XLiSHJ8hJdDII5YnJOSsxforKCtN3jRzXt+ENP5M0VBxD+I6SEG4ZOLp" crossorigin="anonymous"></script>
+<script src="assets/custom/dashboard.js"></script>
+</body>
+</html>
